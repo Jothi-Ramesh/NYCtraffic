896332c165ef2a4d26bc1192917a3f20462408fd
git@github.com <rameshj@wwu.edu>
2022-07-28 00:56:45 -0700

    Aesthetic change

--- nyc-app/src/components/Manhattan.jsx
+++ nyc-app/src/components/Manhattan.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { MapContainer, GeoJSON } from "react-leaflet";
 import districts from './../data/districts.json'
 import "leaflet/dist/leaflet.css";
+import "./Manhattan.css";
 
 class Manhattan extends Component {
 
@@ -9,13 +10,19 @@ class Manhattan extends Component {
         console.log(districts);
     }
 
+
     state = { }
+
+    boroughStyle = {
+        fillColor: "red"
+    }
+
     render() {
         return(
             <div>
                 <h1 style={{textAlign: "center"}}>Manhattan</h1>
                 <MapContainer style = {{height: "80vh"}} zoom = {10} center = {[40.776676, -73.971321]}>
-                    <GeoJSON data={districts.features} />
+                    <GeoJSON /*style={{this.boroughStyle}}*/ data={districts.features} />
                 </MapContainer>
 
             </div>)
