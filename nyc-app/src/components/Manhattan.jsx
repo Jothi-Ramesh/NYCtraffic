eb117187e14fdee9a33a2af5f66be9a71c6bfcba
git@github.com <rameshj@wwu.edu>
2022-08-06 14:59:00 -0700

    added background and all countries

--- nyc-app/src/components/Manhattan.jsx
+++ nyc-app/src/components/Manhattan.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { MapContainer, GeoJSON } from "react-leaflet";
-import districts from './../data/districts.json'
+import mapData from './../data/countries.json'
 import "leaflet/dist/leaflet.css";
 import "./Manhattan.css";
 
 class Manhattan extends Component {
 
     componentDidMount() {
-        console.log(districts);
+        console.log(mapData);
     }
 
 
@@ -21,8 +21,8 @@ class Manhattan extends Component {
         return(
             <div>
                 <h1 style={{textAlign: "center"}}>Manhattan</h1>
-                <MapContainer style = {{height: "80vh"}} zoom = {10} center = {[40.776676, -73.971321]}>
-                    <GeoJSON /*style={{this.boroughStyle}}*/ data={districts.features} />
+                <MapContainer style = {{height: "80vh"}} zoom = {2} center = {[40.776676, -73.971321]}>
+                    <GeoJSON /*style={{this.boroughStyle}}*/ data={mapData.features} />
                 </MapContainer>
 
             </div>)
